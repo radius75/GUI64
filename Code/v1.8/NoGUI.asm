e25aceff09295aba1411b7271df93464bbcf2df0
WebFritzi <70487447+WebFritzi@users.noreply.github.com>
2025-07-21 22:54:31 +0200

    Add files via upload

---
+++ Code/v1.8/NoGUI.asm
@@ -0,0 +1,304 @@
+CopyBlockFBtoFD ldy #0
+-               lda ($fb),y
+                sta ($fd),y
+                dey
+                bne -
+                inc $fc
+                inc $fe
+                dex
+                bne CopyBlockFBtoFD
+                rts
+
+; Puts -A into X
+minus           eor #%11111111
+                tax
+                inx
+                rts
+
+; Adds value in A to FBFC
+AddToFB         clc
+                adc $fb
+                sta $fb
+                bcc +
+                inc $fc
++               rts
+
+; Adds value in A to FDFE
+AddToFD         clc
+                adc $fd
+                sta $fd
+                bcc +
+                inc $fe
++               rts
+
+Val             !byte 0
+SubValFromFD    lda $fd
+                sec
+                sbc Val
+                sta $fd
+                bcs +
+                dec $fe
++               rts
+
+; Adds value in A to 0203
+AddTo02         clc
+                adc $02
+                sta $02
+                bcc +
+                inc $03
++               rts
+
+; Adds BufWidth to FDFE
+AddBufWidthToFD lda BufWidth
+                clc
+                adc $fd
+                sta $fd
+                bcc +
+                inc $fe
++               rts
+
+; Adds BufWidth to 0203
+AddBufWidthTo02 lda BufWidth
+                clc
+                adc $02
+                sta $02
+                bcc +
+                inc $03
++               rts
+
+;Once called, never changes
+SetGlobals      ; Install mouse pointer sprites
+                lda #<SP_Mouse0
+                sta SPRPTR_0
+                lda #<SP_Mouse1
+                sta SPRPTR_1
+                lda #CL_BLACK
+                sta col0
+                lda #CL_WHITE
+                sta col1
+
+                ; Turn on sprites
+                lda #%00111111
+                sta VIC+21
+
+                ; Colors:
+                ; bkg, frame, and multicolor
+                lda #CL_BLACK
+                sta FRAMECOLOR
+                sta BKGCOLOR
+                lda #CL_WHITE
+                sta MULTICOLOR1
+                lda #CL_BLACK
+                sta MULTICOLOR2
+
+                ; Prepare ScrTabLo/Hi, BufScrTabLo/Hi, BufClrTabLo/Hi
+                ldx #0
+                lda #<SCRMEM
+                sta ScrTabLo,x
+                lda #>SCRMEM
+                sta ScrTabHi,x
+                lda #>SCR_BUF
+                sta BufScrTabHi,x
+                lda #>CLR_BUF
+                sta BufClrTabHi,x
+-               lda ScrTabLo,x
+                clc
+                adc #40
+                inx
+                sta ScrTabLo,x
+                ;
+                dex
+                lda ScrTabHi,x
+                inx
+                sta ScrTabHi,x
+                dex
+                lda BufScrTabHi,x
+                inx
+                sta BufScrTabHi,x
+                dex
+                lda BufClrTabHi,x
+                inx
+                sta BufClrTabHi,x
+                bcc +
+                inc ScrTabHi,x
+                inc BufScrTabHi,x
+                inc BufClrTabHi,x
++               cpx #24
+                bcc -
+                
+                ; Set initial values
+                lda #0
+                sta ProgramMode
+                sta AllocedWindows
+                sta VisibleWindows
+                ;
+                lda #<WND_HEAP
+                sta EofWndHeap
+                lda #>WND_HEAP
+                sta EofWndHeap+1
+                ;
+                lda #<CONTROL_HEAP
+                sta EofCtrlsHeap
+                lda #>CONTROL_HEAP
+                sta EofCtrlsHeap+1
+                rts
+
+;---------------------------------------------------------------
+; TIME FUNCTIONS
+;---------------------------------------------------------------
+
+TODInit         lda $01          ;Save $01
+                pha              ; on stack
+                lda #<INT_NMI    ;Setup NMI vector
+                sta $fffa        ; to catch unwanted NMIs
+                lda #>INT_NMI    ;
+                sta $fffb        ;
+                lda #$35         ;Bank out KERNAL
+                sta $01          ; so new NMI vector is active
+
+                lda #0
+                sta $d011        ;Turn off display to disable badlines
+                sta $dc0e        ;Set TOD Clock Frequency to 60Hz
+                sta $dc0f        ;Enable Set-TOD-Clock
+                sta $dc0b        ;Set TOD-Clock to 0 (hours)
+                sta $dc0a        ;- (minutes)
+                sta $dc09        ;- (seconds)
+                sta $dc08        ;- (deciseconds)
+
+                lda $dc08        ;
+-               cmp $dc08        ;Sync raster to TOD Clock Frequency
+                beq -                              
+                
+                ldx #0           ;Prep X and Y for 16 bit
+                ldy #0           ; counter operation
+                lda $dc08        ;Read deciseconds
+-               inx              ;2   -+
+                bne +            ;2/3  | Do 16 bit count up on
+                iny              ;2    | X(lo) and Y(hi) regs in a 
+                jmp ++           ;3    | fixed cycle manner
++               nop              ;2    |
+                nop              ;2   -+
+++              cmp $dc08        ;4 - Did 1 decisecond pass?
+                beq -            ;3 - If not, loop-di-doop
+                                 ;Each loop = 16 cycles
+                                 ;If less than 118230 cycles passed, TOD is 
+                                 ;clocked at 60Hz. If 118230 or more cycles
+                                 ;passed, TOD is clocked at 50Hz.
+                                 ;It might be a good idea to account for a bit
+                                 ;of slack and since every loop is 16 cycles,
+                                 ;28*256 loops = 114688 cycles, which seems to be
+                                 ;acceptable. That means we need to check for
+                                 ;a Y value of 28.
+
+                cpy #28          ;Did 114688 cycles or less go by?
+                bcc +            ;- Then we already have correct 60Hz $dc0e value
+                lda #$80         ;Otherwise, we need to set it to 50Hz
+                sta $dc0e
++               lda #$1b         ;Enable the display again
+                sta $d011
+
+                pla              ;Restore old $01 value
+                sta $01          ; and potentially old NMI vector
+                rts                             
+INT_NMI         rti
+
+SetTOD          lda $dc0f
+                and #%01111111
+                sta $dc0f
+                ;
+                lda Clock+2
+                asl
+                asl
+                asl
+                asl
+                ora Clock+3
+                sta $dc0a
+                ;
+                lda Clock
+                asl
+                asl
+                asl
+                asl
+                ora Clock+1
+                ;
+                bne +
+                lda #$92; actually $12
+                sta $dc0b
+                jmp ++
++               cmp #$12
+                bcs +
+                sta $dc0b
+                jmp ++
++               bne +
+                sta $dc0b
+                jmp ++
++               sed
+                sec
+                sbc #$12
+                cld
+                ora #%10000000
+                sta $dc0b
+++              lda #0
+                sta $dc08
+                sta $dc09
+                rts
+
+DisplayClock    lda MayShowClock
+                beq ++
+                lda #':'
+                sta SCRMEM+23*40+34+2
+
+                lda $dc0a
+                tax
+                lsr
+                lsr
+                lsr
+                lsr
+                sta Clock+2
+                ora #$30
+                sta SCRMEM+23*40+37
+                txa
+                and #%00001111
+                sta Clock+3
+                ora #$30
+                sta SCRMEM+23*40+38
+                
+                lda $dc0b
+                ldx $dc08
+                tax
+                and #%01111111
+                cmp #$12
+                bne +
+                
+                ; 12 pm = noon or 12 am = midnight
+                txa
+                and #%10000000
+                tax
+                
++               txa
+                and #%10000000
+                beq am
+                ; pm
+                txa
+                and #%01111111
+                sed
+                clc
+                adc #$12
+                cld
+                tax
+                jmp +
+am              txa
+                and #%01111111
++               lsr
+                lsr
+                lsr
+                lsr
+                sta Clock
+                ora #$30
+                sta SCRMEM+23*40+34
+                txa
+                and #%00001111
+                sta Clock+1
+                ora #$30
+                sta SCRMEM+23*40+35
+++              rts
---
+++ Code/v1.8/StringRoutines.asm
@@ -0,0 +1,542 @@
+; Prints multi-line string from FBFC to FDFE
+; with lower case conversion
+; FDFE needs to be in Screen Buffer
+PrintStringLC_ML
+                lda $fe
+                cmp #$ff
+                beq ++
+--              ldy #$ff
+-               iny
+                lda ($fb),y
+                beq ++
+                cmp #28; carriage return
+                bne +
+                iny
+                sty string_width_plus_1
+                tya
+                jsr AddToFB
+                dey
+                jsr AddBufWidthToFD
+                jmp --
++               jsr PetLCtoDesktop
+                sta ($fd),y
+                jmp -
+++              rts
+
+; For multi-line strings:
+; Finds StringWidth and StringHeight
+; of string in FBFC
+GetStringInfo   lda #0
+                sta StringWidth
+                sta StringHeight
+                ldx #0; counts string lines
+--              ldy #$ff
+-               iny
+                lda ($fb),y
+                beq +
+                cmp #28; carriage return
+                bne -
+                ; char is carriage return
+                iny
+                sty string_width_plus_1
+                tya
+                jsr AddToFB
+                ;+AddByteToFB string_width_plus_1
+                dey
+                inx
+                cpy StringWidth
+                bcc --
+                sty StringWidth
+                jmp --
++               inx
+                stx StringHeight
+                cpy StringWidth
+                bcc +
+                sty StringWidth
++               rts
+string_width_plus_1    !byte 0
+
+; Prints string from FBFC to FDFE
+; with lower case conversion
+; and returns string len in res
+PrintStringLC   ldy #0
+                lda $fe
+                cmp #$ff
+                beq +
+                ldy #$ff
+-               iny
+                lda ($fb),y
+                beq +
+                jsr PetLCtoDesktop
+                sta ($fd),y
+                jmp -
++               sty res
+                rts
+
+; Prints string from FBFC to FDFE
+; with upper case conversion
+; and returns string len in res
+PrintStringUC   ldy #0
+                lda $fe
+                cmp #$ff
+                beq +
+                ldy #$ff
+-               iny
+                lda ($fb),y
+                beq +
+                jsr PetUCtoDesktop
+                sta ($fd),y
+                jmp -
++               sty res
+                rts
+
+; Prints string from FBFC to FDFE with max length in Param
+; Param+1=0: Lower case, Param+1=1: Upper case
+; If string is too long, it terminates with "..."
+PrintStrMaxLen  dec Param
+                bmi +++
+                lda $fc
+                cmp #$ff
+                beq +++
+                ldy #$ff
+-               iny
+                lda ($fb),y
+                beq +++
+                ldx Param+1
+                beq +
+                jsr PetUCtoDesktop
+                jmp ++
++               jsr PetLCtoDesktop
+++              sta ($fd),y
+                cpy Param
+                bcc -
+                iny
+                lda ($fb),y
+                beq +++
+                dey
+                lda #219
+                sta ($fd),y
++++             rts
+
+; Prints string from FDFE to FBFC with max length in Param
+; Param+1 indicates whether it's a drive window or not
+; If string is too long, it terminates with "..."
+PrintStrTaskbar dec Param
+                bmi +++
+                lda $fe
+                cmp #$ff
+                beq +++
+                ldy #0
+                lda ($fd),y
+                beq +++
+                ; Prepare copy char
+                lda #<TASKCHARBASE
+                sta smc1+1
+                lda #>TASKCHARBASE
+                sta smc2+1
+                lda #<TB_Reserved
+                sta smc3+1
+                lda #>TB_Reserved
+                sta smc4+1
+                lda #TB_Reserved_Char
+                sta smc5+1
+                dey
+--              iny
+                lda ($fd),y
+                beq ++
+                ldx Param+1
+                beq no_drv
+                jsr PetUCtoTaskbar
+                jmp +
+no_drv          jsr PetLCtoTaskbar
++               ldx TaskBtnPressed
+                beq +
+                ; Press char
+                ;jsr MapOutIO
+                jsr CopyCharToReserved
+                ;jsr MapInIO
++               sta ($fb),y
+                cpy Param
+                bcc --
+                iny
+                lda ($fd),y
+                beq ++
+                dey
+                lda #91
+                ldx TaskBtnPressed
+                beq +
+                ;jsr MapOutIO
+                jsr CopyCharToReserved
+                ;jsr MapInIO
++               sta ($fb),y
+++              lda TaskBtnPressed
+                beq +++
+                ;jsr MapOutIO
+                jsr PressReserved_TB
+                ;jsr MapInIO
++++             rts
+
+Old01           !byte 0
+MapOutIO        ldx $01
+                stx Old01
+                ldx #$34
+                stx $01
+                rts
+
+MapInIO         ldx Old01
+                stx $01
+                rts
+
+; Copies char in A to y-th position in Reserved
+; Expects A, smc1+1, smc2+1, smc3+1, smc4+1 filled
+; smc5+1 is optional and should be filled with 
+; DT_Reserved_Char or TB_Reserved_Char
+CopyCharToReserved
+                ; Find address in char set
+                sta $02
+                lda #0
+                sta $03
+                asl $02
+                rol $03
+                asl $02
+                rol $03
+                asl $02
+                rol $03
+                lda $02
+                clc
+smc1            adc #0;Lobyte of char set
+                sta $02
+                lda $03
+smc2            adc #0;Hibyte of char set
+                sta $03
+                ; Find y pos in Reserved
+                tya
+                asl
+                asl
+                asl
+                clc
+smc3            adc #0;Lobyte of Reserved
+                sta $04
+smc4            lda #0;Hibyte of Reserved
+                adc #0
+                sta $05
+                ; Copy
+                sty dummy
+                ldy #7
+-               lda ($02),y
+                sta ($04),y
+                dey
+                bpl -
+                ldy dummy
+                tya
+                clc
+smc5            adc #0
+                rts
+
+PressReserved_TB
+                lda #<TB_Reserved
+                sta $fb
+                lda #>TB_Reserved
+                sta $fc
+                ldx #1
+--              ldy #7
+-               lda ($fb),y
+                lsr
+                sta ($fb),y
+                dey
+                bpl -
+                ldy #7
+-               dey
+                lda ($fb),y
+                iny
+                sta ($fb),y
+                dey
+                bne -
+                ldy #0
+                lda #0
+                sta ($fb),y
+                lda #8
+                jsr AddToFB
+                inx
+                cpx #11
+                bcc --
+                rts
+
+PressReserved_DT
+                lda #<DT_Reserved
+                sta $fb
+                lda #>DT_Reserved
+                sta $fc
+                ldx ControlWidth
+                dex
+                dex
+--              ; Shift right
+                ldy #7
+-               lda ($fb),y
+                lsr
+                ora #%10000000
+                sta ($fb),y
+                dey
+                bpl -
+                ; Push down
+                ldy #7
+                lda ($fb),y
+                ldy #0
+                sta ($0c),y                
+                ldy #7
+-               dey
+                lda ($fb),y
+                iny
+                sta ($fb),y
+                dey
+                bne -
+                lda #$ff
+                sta ($fb),y
+                lda #8
+                jsr AddToFB
+                ;+AddValToFB 8
+                +AddValTo0c 8
+                dex
+                bne --
+                rts
+
+InvertReserved_DT
+                lda #16
+                ; Get number of bytes to invert
+                asl
+                asl
+                asl
+                tay
+                dey
+                ; Invert
+                jsr MapOutIO
+                lda #<DT_Reserved
+                sta $fb
+                lda #>DT_Reserved
+                sta $fc
+-               lda ($fb),y
+                eor #%11111111
+                sta ($fb),y
+                dey
+                bne -
+                lda ($fb),y
+                eor #%11111111
+                sta ($fb),y
+                jsr MapInIO
+                rts
+
+GenerateReservedForSM
+                lda #<Menu_Start
+                sta $fb
+                lda #>Menu_Start
+                sta $fc
+                lda #3
+                jsr AddToFB
+                lda #<DT_Reserved
+                sta smc3+1
+                lda #>DT_Reserved
+                sta smc4+1
+                lda #<CHARBASE
+                sta smc1+1
+                lda #>CHARBASE
+                sta smc2+1
+                ;
+                jsr MapOutIO
+                ldx #16
+                ldy #0
+-               lda ($fb),y
+                bne +
+                lda #3
+                jsr AddToFB
+                jmp -
++               jsr PetLCtoDesktop
+                jsr CopyCharToReserved
+                iny
+                cpy #16
+                bcc -
+                jsr MapInIO
+                rts
+
+; Sets FDFE to next string and returns
+; strlen of previous string in res
+NextString      ldy #$ff
+-               iny
+                lda ($fd),y
+                bne -
+                iny
+                sty dummy
+                tya
+                jsr AddToFD
+                ;+AddByteToFD dummy
+                dey
+                sty res
+                rts
+
+; Gets length of string in FDFE
+; Output in res (or Y)
+GetStrLen       ldy #$ff
+-               iny
+                lda ($fd),y
+                bne -
+                sty res
+                rts
+
+; Prints string from FDFE to FBFC
+; with lower case conversion
+; and writes strlen to res
+PrintIntString  ldy #0
+-               lda ($fd),y
+                beq +
+                jsr PetLCtoDesktop
+                sta ($fb),y
+                iny
+                jmp -
++               sty res
+                rts
+
+PetUCtoDesktop  cmp #32
+                bcs +
+                lda #191
+                rts
++               cmp #64
+                bcs +
+                ; 32 <= a < 64
+                ora #%10000000
+                rts
++               bne +
+                ; a = 64
+                lda #192
+                rts
++               cmp #96
+                bcs +
+                ; 65 <= a < 96
+                clc
+                adc #64
+                rts
++               cmp #123
+                bcs +
+                ; 96 <= a < 123
+                clc
+                adc #96
+                rts
++               cmp #193
+                bcs +
+                ; 123 <= a < 193
+                lda #191
+                rts
++               cmp #219
+                bcs +
+                ; 193 <= a < 219
+                rts
++               ; 219 <= a
+                lda #191
+                rts
+
+PetLCtoDesktop  cmp #32
+                bcs +
+                lda #191
+                rts
++               cmp #91
+                bcs +
+                ; 32 <= a < 91
+                ora #%10000000
+                rts 
++               cmp #96
+                bcs +
+                ; 91 <= a < 96
+                clc
+                adc #64
+                rts
++               cmp #123
+                bcs +
+                ; 96 <= a < 123
+                clc
+                adc #32
+                rts
++               cmp #192
+                bcs +
+                ; 123 <= a < 192
+                lda #191
+                rts
++               cmp #219
+                bcs +
+                ; 192 <= a < 218
+                sec
+                sbc #64
+                rts
++               ; 219 <= a
+                ;lda #191
+                sec
+                sbc #172
+                rts
+
+PetUCtoTaskbar  cmp #32
+                bcs +
+                lda #63
+                rts
++               cmp #65
+                bcs +
+                ; 32 <= a < 65
+                rts
++               cmp #96
+                bcs +
+                ; 65 <= a < 96
+                and #%00111111
+                rts
++               cmp #123
+                bcs +
+                ; 96 <= a < 123
+                sec
+                sbc #32
+                rts
++               cmp #192
+                bcs +
+                ; 123 <= a < 192
+                lda #63
+                rts
++               cmp #219
+                bcs +
+                ; 192 <= a < 218
+                and #%01111111
+                rts
++               ; 218 <= a
+                lda #63
+                rts
+
+PetLCtoTaskbar  cmp #32
+                bcs +
+                lda #63
+                rts
++               cmp #91
+                bcs +
+                ; 32 <= a < 91
+                rts
++               cmp #96
+                bcs +
+                ; 91 <= a < 96
+                and #%10111111
+                rts
++               bne +
+                ; a = 96
+                lda #45
+                rts
++               cmp #123
+                bcs +
+                ; 97 <= a < 123
+                sec
+                sbc #96
+                rts
++               cmp #193
+                bcs +
+                ; 123 <= a < 193
+                lda #63
+                rts
++               cmp #219
+                bcs +
+                ; 193 <= a < 218
+                sec
+                sbc #192
+                rts
++               ; 219 <= a
+                lda #63
+                rts
